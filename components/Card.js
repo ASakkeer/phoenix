4477cc0f018bc723c056363452b5e706b3644ef2
SakkeerA <sakkeer@mongrov.com>
2022-05-06 20:17:55 +0530

    Card price UI - stable

--- components/Card.js
+++ components/Card.js
@@ -13,10 +13,31 @@ const _styles = StyleSheet.create({
   },
   title: {
     color: 'grey',
+    fontSize: 12,
+    marginBottom: 5,
   },
   mainTitle: {
     color: '#4a6076',
-    fontSize: 16,
+    // fontSize: 16,
+    marginBottom: 5,
+  },
+  currentPriceLabel: {
+    color: '#000',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  pastPriceLabel: {
+    color: 'grey',
+    fontSize: 12,
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+    marginRight: 5,
+  },
+  discountLabel: {
+    color: '#4a6076',
+    fontWeight: 'bold',
+    fontSize: 12,
   },
 });
 
@@ -46,6 +67,50 @@ function CardView(props) {
           <Text style={_styles.mainTitle} numberOfLines={1}>
             {itemDetails.productName}
           </Text>
+        </View>
+        {/* Size UI start */}
+        {/* {itemDetails?.size.length > 0 && (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+            }}>
+            {itemDetails?.size.map(eachSize => (
+              <View
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#4a6076',
+                  borderRadius: 100,
+                  width: 35,
+                  height: 35,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  marginRight: 5,
+                  padding: 3,
+                }}>
+                <Text style={{fontSize: 16, color: '#4a6076'}}>{eachSize}</Text>
+              </View>
+            ))}
+          </View>
+        )} */}
+        {/* Size UI end */}
+
+        <View></View>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            // justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={_styles.currentPriceLabel}>
+            <Text style={{fontSize: 12}}>{'₹'}</Text>
+            {itemDetails?.currentPrice}
+          </Text>
+          <Text style={_styles.pastPriceLabel}>{itemDetails?.pastPrice}</Text>
+          <Text style={_styles.discountLabel}>{itemDetails?.discount} off</Text>
         </View>
       </View>
     </View>
